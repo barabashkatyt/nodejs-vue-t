0f67ebc6deefa31deab88591f9e993b1073b2670
balbatushka <balbatushka@gmail.com>
2020-12-21 15:52:18 +0300

    vue nodejs rest tutorial

---
+++ client/frontend.js
@@ -0,0 +1,31 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
+
+new Vue({
+  el: "#app",
+  data() {
+    return {
+      form: {
+        name: "",
+        value: "",
+      },
+      contacts: [
+        { id: 1, name: "Vasili", value: "456654654654", marked: false },
+      ],
+    };
+  },
+  methods: {
+    createContact() {
+      const { ...contact } = this.form;
+      this.form.name = this.form.value = "";
+      this.contacts.push({ ...contact, id: Date.now() });
+    },
+    markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      console.log(id);
+      console.log(contact);
+      contact.marked = true;
+      console.log(contact.marked);
+    },
+    removeContact() {},
+  },
+});
