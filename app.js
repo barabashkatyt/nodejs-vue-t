34694875f91ae1cd6de9c182ccd20d883beb80bd
balbatushka <balbatushka@gmail.com>
2020-12-23 08:29:50 +0300

    loader added, id npw from unique id

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const path = require("path");
+const { v4 } = require("uuid");
 const app = express();
+
+const CONTACTS = [
+  { id: v4(), name: "Vasili", value: "456654654654", marked: false },
+];
+
+//GET
+app.get("/api/contacts", (req, res) => {
+  setTimeout(() => {
+    res.status(200).json(CONTACTS);
+  }, 1000);
+});
+
+//POST
+app.post("/api/contacts", (req, res) => {});
 
 app.use(express.static(path.resolve(__dirname, "client")));
 
